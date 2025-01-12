5208ae494dcb2c5bd767e4ebca7c557bbd1f4de2
rh3as <rheasusarla@gmail.com>
2025-01-12 11:26:49 -0800

    More changes

--- 22visiontester.py
+++ 22visiontester.py
@@ -16,29 +16,25 @@ def px_to_deg(cx, cy):
 def draw_point(image, x, y):
     cv2.circle(image, (int(x), int(y)), 5, (255, 0, 0), cv2.FILLED)
 
-# runPipeline() is called every frame by Limelight's backend.
 def runPipeline(frame):
-    # Convert the input frame to the HSV color space
     img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     upper = np.array([90, 200, 255], dtype=np.uint8)
     lower = np.array([75, 75, 30], dtype=np.uint8)
     img_threshold = cv2.inRange(img_hsv, lower, upper)
 
-    # Save the mask image for debugging purposes
     cv2.imwrite("mask.jpg", img_threshold)
 
-    # Find contours in the binary image
     contours, _ = cv2.findContours(
         img_threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
     )
-    
+
     for i in range(len(contours)):
         if cv2.contourArea(contours[i]) < 4:
             continue
 
         (x, y), radius = cv2.minEnclosingCircle(contours[i])
 
-        # filter by the radius size
+        # filtesr by the radius size
         if radius < 50:
             continue
 
@@ -46,7 +42,7 @@ def runPipeline(frame):
         radius = int(radius)
         cv2.circle(frame, center, radius, (0, 255, 0), 2)  
         
-        draw_point(frame, x, y)  # Draw the center point
+        draw_point(frame, x, y) 
 
         # pixels to angles
         tx, ty = px_to_deg(x, y)
@@ -57,19 +53,16 @@ def runPipeline(frame):
 if __name__ == "__main__":
     startTime = time.perf_counter()
 
-    # Load the test image
     img = cv2.imread("./testimgs/4.jpg")
 
-    # Apply Gaussian blur
+    # maybe remove this
     img = cv2.GaussianBlur(img, (101, 101), 0)
 
-    # Run the pipeline
     threshold_img, processed_img = runPipeline(img)
 
     endTime = time.perf_counter()
     print(f"Processing time: {endTime - startTime} seconds")
 
-    # Display the threshold and processed images
     cv2.imshow("Threshold", threshold_img)
     cv2.imshow("Processed", processed_img)
     cv2.waitKey(0)
