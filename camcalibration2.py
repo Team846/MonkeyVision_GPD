b180b743755bc878f6dd8a72b2affa67e6ed5300
rh3as <rheasusarla@gmail.com>
2025-01-31 18:55:15 -0800

    Co-authored-by: akshaylakkur <akshaylakkur@users.noreply.github.com> Co-authored-by: Aravindkrishna Arivudainambi <aravindkrishna2008@users.noreply.github.com>

--- camcalibration2.py
+++ camcalibration2.py
@@ -2,12 +2,14 @@ import numpy as np
 import cv2 as cv
 import glob
 import os
+import json
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((7*10,3), np.float32)
+objp[:,:2] = np.mgrid[0:10,0:7].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -18,14 +20,13 @@ cam = cv.VideoCapture(0)
 # images = glob.glob('*.jpg')
 while True:
     ret, img = cam.read()
-    cv.imshow("img", img)
 
     if img is None:
         print("No image")
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # cv.imwrite('img.jpg', img)
     # Find the chess board corners
-    ret, corners =  cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners =  cv.findChessboardCorners(gray, (7,10), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -33,68 +34,9 @@ while True:
 
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
-        print(ret, corners)
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        cv.imwrite('img.jpg', img)
+
+        cv.drawChessboardCorners(img, (7,10), corners2, ret)
         break
-    # else:
-    #     print(ret, corners)
 
-
-
-    #     # Draw and display the corners
-
-    # cv.imwrite('img.jpg', img)
-    # cv.waitKey(500)
-
+    cv.imwrite('./calibration/img8.jpg', img)
     cv.destroyAllWindows()
-
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-# undistort
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('calibresult.png', dst)
-
-# undistort
-mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-
-
-# # termination criteria
-# criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
- 
-# # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-# objp = np.zeros((6*7,3), np.float32)
-# objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
- 
-# # Arrays to store object points and image points from all the images.
-# objpoints = [] # 3d point in real world space
-# imgpoints = [] # 2d points in image plane.
- 
-# images = glob.glob('*.png')
- 
-# for fname in images:
-#     img = cv.imread(fname)
-#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
- 
-#     # Find the chess board corners
-#     ret, corners = cv.findChessboardCorners(gray, (6,7), None)
- 
-#     # If found, add object points, image points (after refining them)
-#     if ret == True:
-#         objpoints.append(objp)
- 
-#         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-#         imgpoints.append(corners2)
- 
-#         # Draw and display the corners
-#         cv.drawChessboardCorners(img, (7,6), corners2, ret)
-#         cv.imshow('img', img)
-#         cv.waitKey(500)
- 
-# cv.destroyAllWindows()
---
+++ undistort.py
@@ -0,0 +1,33 @@
+import numpy as np
+import json 
+import cv2
+
+with open('matrix.json', 'r') as f:
+    data = json.load(f)
+    
+camera_matrices = []
+dist_coeffs = []
+
+for calib in data:
+    cam_mat = np.array(calib['mtx'])
+    dist_coef = np.array(calib['dist'])
+    
+    camera_matrices.append(cam_mat)
+    dist_coeffs.append(dist_coef)
+    
+avg_camera_matrix = np.mean(camera_matrices, axis=0)
+avg_dist_coeffs = np.mean(dist_coeffs, axis=0)
+
+print("Averaged Camera Matrix:\n", avg_camera_matrix)
+print("Averaged Distortion Coefficients:\n", avg_dist_coeffs)
+
+img = cv2.imread('img.jpg')
+if img is None:
+    raise IOError("Image not found. Please check the file path.")
+
+h, w = img.shape[:2]
+
+new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(avg_camera_matrix, avg_dist_coeffs, (w, h), 1, (w, h))
+
+undistorted_img = cv2.undistort(img, avg_camera_matrix, avg_dist_coeffs, None, new_camera_matrix)
+cv2.imwrite('undistorted.jpg', undistorted_img)
